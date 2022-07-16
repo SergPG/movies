82cf1b2e3c16dc77996f2151939f2d6feb8f794d
SergPG <serg-popov@ukr.net>
2022-07-16 19:31:28 +0300

    Working with CSS

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,37 @@
 class MoviesController < ApplicationController
   def index
-    @movie = Movie.all
+    respond_to do |format|
+      format.html do
+        render :index, locals: {categories:, movies: movies }
+      end
+      format.js { render :index, locals: { movies: movies } }
+    end
   end
+
+  def show
+    render :show, locals: { movie: }
+  end
+
+
+  private
+
+    def filter_params
+      params.permit(:category_id)
+    end
+   
+   def movies
+    @movies ||= Movie.where(filter_params)
+                     .order(id: :desc)
+                     .limit(6)
+   end
+
+   def movie
+    @movie ||= Movie.find(params[:id])
+   end
+
+   def categories
+    @categories ||= Category.all
+   end
+
+
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -13,6 +13,7 @@ exampleModal.addEventListener('show.bs.modal', function (event) {
   var button = event.relatedTarget
   // Extract info from data-bs-* attributes
   var recipient = button.getAttribute('data-bs-whatever')
+  console.log(recipient)  
   // If necessary, you could initiate an AJAX request here
   // and then do the updating in a callback.
   //
@@ -23,3 +24,26 @@ exampleModal.addEventListener('show.bs.modal', function (event) {
   modalTitle.textContent = 'New message to ' + recipient
   modalBodyInput.value = recipient
 })
+
+//=======================
+var categoryFilter = document.getElementById('category_filter')
+categoryFilter.addEventListener('change', function (event) {
+  console.log($(this).val())
+  // $.ajax({
+  //   url: 'movies',
+  //   type: 'GET',
+  //   dataType: 'script',
+  //   data: { category_id: $(this).val() }
+  // });
+}) 
+
+// $('#category_filter').on('change', function(){
+//   console.log($(this).val())
+//  // $.ajax({
+//  //     url: 'movies',
+//  //     type: 'GET',
+//  //     dataType: 'script',
+//  //     data: { category_id: $(this).val() }
+//  // });
+// });
+
