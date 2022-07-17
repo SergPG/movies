5f68b69bda7efbaa10e543fbef2fb7cad441af04
SergPG <serg-popov@ukr.net>
2022-07-17 20:19:55 +0300

    Working rating

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :rememberable, :validatable
-  
+        
   rating
-
-  def admin?
-    true
-  end
 end
