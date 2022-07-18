b5f80feb37caa79d5e71d382e6ea9fda5fe8650f
SergPG <serg-popov@ukr.net>
2022-07-18 08:39:01 +0300

    Working testing

--- spec/controllers/movis_controller_spec.rb
+++ spec/controllers/movis_controller_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.describe MoviesController, type: :controller do
    
     describe 'GET #show ' do
-        let!(:movie) {create :movie }
-        # before { get :show }
-
-        # it { subject render_template('show') }
-
+         let!(:movie) {create :movie }
+         let(:params) { { id: movie.id } }
+       
+      it "returns a 200" do
+         get :show, params: params   
+         expect(response).to have_http_status(:ok) 
+      end  
     end    
-
 end
